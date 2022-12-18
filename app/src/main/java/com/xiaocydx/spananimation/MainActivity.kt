298a65bf2650f4408824038a38c3f571b7329d31
xcc <xiaocydx@163.com>
2022-12-18 12:19:47 +0800

    init commit

---
+++ app/src/main/java/com/xiaocydx/spananimation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.xiaocydx.spananimation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
